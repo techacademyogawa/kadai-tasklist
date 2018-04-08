5073d99df7252ea1cc1965b96fe57e8f21ba82eb
EC2 Default User <ec2-user@ip-172-31-8-179.us-east-2.compute.internal>
2018-04-08 12:20:17 +0000

    CRUD tasks

--- config/app.php
+++ config/app.php
@@ -144,6 +144,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -193,6 +195,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
